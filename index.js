9f96e035a7c4633e432a85023620b2590e8df6bb
RoufRain <roufrain91@gmail.com>
2023-11-07 17:24:48 +0600

    added .env file

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(express.json());
 //U2U0UTE990MIsBap
 //hotelManagement
 
-const uri = `mongodb+srv://hotelManagement:U2U0UTE990MIsBap@cluster0.3ovngzi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ovngzi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
